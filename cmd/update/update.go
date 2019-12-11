9ed26753371d7d17121aed3f174841c3856f1474
Jeremie Rodriguez <contact@jeremierodriguez.com>
2019-12-11 15:19:15 +0100

    feat(update): improve update command

--- cmd/update/update.go
+++ cmd/update/update.go
@@ -1,37 +1,76 @@
 package update
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strconv"
 
+	"github.com/blang/semver"
 	"github.com/spf13/cobra"
+	"gitlab.com/jeremiergz/nas-cli/cmd/info"
 	"gitlab.com/jeremiergz/nas-cli/util"
 	"gitlab.com/jeremiergz/nas-cli/util/console"
 )
 
+// GitLabTag describes tag resource (https://docs.gitlab.com/ee/api/tags.html)
+type GitLabTag struct {
+	Message   string `json:"message"`
+	Name      string `json:"name"`
+	Protected bool   `json:"protected"`
+	Target    string `json:"target"`
+}
+
+func isLatest(version string) bool {
+	isLatest := false
+	getTagsURL := fmt.Sprintf("%s/projects/%s/repository/tags?sort=desc", glAPIURL, strconv.Itoa(glProjectID))
+	res, err := http.Get(getTagsURL)
+	if err == nil {
+		defer res.Body.Close()
+		var tags []GitLabTag
+		json.NewDecoder(res.Body).Decode(&tags)
+		if len(tags) > 0 {
+			latestVersion, _ := semver.Make(tags[0].Name)
+			currentVersion, _ := semver.Make(version)
+			isLatest = currentVersion.GTE(latestVersion)
+		}
+	}
+	return isLatest
+}
+
 const (
-	glProjectURL    = "https://gitlab.com/jeremiergz/nas-cli"
-	glArtifactURL   = "-/jobs/artifacts/master/raw"
 	glArtifactQuery = "?job=build:go"
+	glArtifactURL   = "-/jobs/artifacts/master/raw"
+	glBaseURL       = "https://gitlab.com"
+	glProjectID     = 12609643
 )
 
 var (
+	glAPIURL     = fmt.Sprintf("%s/api/v4", glBaseURL)
+	glProjectURL = fmt.Sprintf("%s/jeremiergz/nas-cli", glBaseURL)
+
 	// Cmd updates the application
 	Cmd = &cobra.Command{
 		Use:   "update",
 		Short: "Update application",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			executablePath, _ := filepath.Abs(os.Args[0])
-			artifactDownloadURL := fmt.Sprintf("%s/%s/nas-cli-%s-%s%s", glProjectURL, glArtifactURL, runtime.GOOS, runtime.GOARCH, glArtifactQuery)
-			fmt.Println("Downloading latest version...")
-			err := util.DownloadFile(executablePath, artifactDownloadURL)
-			if err == nil {
-				os.Chmod(executablePath, 0755)
-				console.Success("Update successful")
+			if isLatest(info.Version) {
+				fmt.Println("Already using latest version")
+			} else {
+				executablePath, _ := filepath.Abs(os.Args[0])
+				artifactDownloadURL := fmt.Sprintf("%s/%s/nas-cli-%s-%s%s", glProjectURL, glArtifactURL, runtime.GOOS, runtime.GOARCH, glArtifactQuery)
+				fmt.Println("Downloading latest version...")
+				err := util.DownloadFile(executablePath, artifactDownloadURL)
+				if err == nil {
+					os.Chmod(executablePath, 0755)
+					console.Success("Update successful")
+				}
+				return err
 			}
-			return err
+			return nil
 		},
 	}
 )
--- go.mod
+++ go.mod
@@ -4,18 +4,12 @@ go 1.13
 
 require (
 	github.com/DiSiqueira/GoTree v1.0.0
+	github.com/blang/semver v3.5.1+incompatible
 	github.com/cavaliercoder/grab v2.0.0+incompatible
 	github.com/cheggaaa/pb v2.0.7+incompatible
 	github.com/cheggaaa/pb/v3 v3.0.3
-	github.com/manifoldco/promptui v0.3.2
+	github.com/manifoldco/promptui v0.6.0
 	github.com/middelink/go-parse-torrent-name v0.0.0-20190301154245-3ff4efacd4c4
 	github.com/spf13/cobra v0.0.5
-	github.com/spf13/viper v1.5.0
-	gopkg.in/VividCortex/ewma.v1 v1.1.1 // indirect
-	gopkg.in/alecthomas/kingpin.v3-unstable v3.0.0-20191105091915-95d230a53780 // indirect
-	gopkg.in/cheggaaa/pb.v2 v2.0.7 // indirect
-	gopkg.in/fatih/color.v1 v1.7.0 // indirect
-	gopkg.in/mattn/go-colorable.v0 v0.1.0 // indirect
-	gopkg.in/mattn/go-isatty.v0 v0.0.4 // indirect
-	gopkg.in/mattn/go-runewidth.v0 v0.0.4 // indirect
+	github.com/spf13/viper v1.6.1
 )
