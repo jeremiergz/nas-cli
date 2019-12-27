6da3dad3ab40062e9a3b62c33690b79e44f6f932
Jeremie Rodriguez <contact@jeremierodriguez.com>
2019-12-27 20:00:30 +0100

    fix(info): fix build date being dynamic instead of static

--- cmd/info/info.go
+++ cmd/info/info.go
@@ -3,14 +3,13 @@ package info
 import (
 	"fmt"
 	"runtime"
-	"time"
 
 	"github.com/spf13/cobra"
 )
 
 var (
 	// BuildDate is the last build datetime, overriden as ldflag
-	BuildDate = time.Now().UTC().Format(time.RFC3339)
+	BuildDate = ""
 
 	// Compiler is the the compiler toolchain that built the running binary
 	Compiler = fmt.Sprintf("%s/%s", runtime.Compiler, runtime.Version())
@@ -22,7 +21,7 @@ var (
 	Platform = fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)
 
 	// Version is the Calendar Versioning string, overriden as ldflag
-	Version = "0.1.0"
+	Version = ""
 
 	// Cmd prints application information
 	Cmd = &cobra.Command{
