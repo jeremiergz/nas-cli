059c81812614bc707fb77660c64af564217a9c4c
Jeremie Rodriguez <contact@jeremierodriguez.com>
2021-11-16 14:34:33 +0100

    refactor(completion): visual code tweaks

--- cmd/completion/completion.go
+++ cmd/completion/completion.go
@@ -9,13 +9,14 @@ import (
 var Cmd = &cobra.Command{
 	Use:       "completion <bash|zsh>",
 	Short:     "Generate completion scripts",
-	Args:      cobra.ExactValidArgs(1),
 	ValidArgs: []string{"bash", "zsh"},
+	Args:      cobra.ExactValidArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		rootCmd := cmd.Parent()
 		switch args[0] {
 		case "bash":
 			return rootCmd.GenBashCompletion(os.Stdout)
+
 		case "zsh":
 			return rootCmd.GenZshCompletion(os.Stdout)
 		}
