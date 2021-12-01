7a6358e708f1bb54b2812dbd40bb2c1729193c1c
Jeremie Rodriguez <contact@jeremierodriguez.com>
2021-12-01 03:44:25 +0100

    chore(homebrew): add formula file

---
+++ tap/Formula/nas-cli.rb
@@ -0,0 +1,18 @@
+class NasCli < Formula
+  version "21.11.11"
+  desc "CLI application for managing my NAS."
+  homepage "https://github.com/jeremiergz/nas-cli"
+  url "https://github.com/jeremiergz/nas-cli/releases/download/#{version}/nas-cli-darwin-amd64"
+  head "https://github.com/jeremiergz/nas-cli.git"
+  license "Apache-2.0"
+  conflicts_with "nas-cli"
+  sha256 "2561fd6705927e4bae44139534505296fd672bf9c348f8be0eeadce6749b1baf"
+
+  def install
+    bin.install "nas-cli-darwin-amd64" => "nas-cli"
+  end
+
+  test do
+    system "#{bin}/nas-cli version"
+  end
+end
